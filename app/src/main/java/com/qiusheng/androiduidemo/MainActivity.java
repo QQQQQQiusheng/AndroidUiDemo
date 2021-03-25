9e03b0da6ffb61744c6bf3897fefaa75fc75c3c7
1654938247@qq.com <1654938247@qq.com>
2021-03-25 09:04:34 +0800

    测试Git

---
+++ app/src/main/java/com/qiusheng/androiduidemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.qiusheng.androiduidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.ev_demo);
+        TextView textView_btn = (TextView) findViewById(R.id.tv_btn_demo);
+        ImageView imageView = (ImageView) findViewById(R.id.iv_demo);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+
+        textView_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edit_text = editText.getText().toString();
+
+                switch (v.getId()) {
+                    case R.id.tv_btn_demo:
+                        imageView.setImageResource(R.mipmap.pic2);
+                        Toast.makeText(MainActivity.this, edit_text, Toast.LENGTH_SHORT).show();
+                      /*  if (progressBar.getVisibility() == View.GONE) {
+                            progressBar.setVisibility(View.VISIBLE);
+                        } else {
+                        progressBar.setVisibility(View.GONE);
+                        }*/
+                        int pro = progressBar.getProgress();
+                        pro = pro + 20;
+                        progressBar.setProgress(pro);
+
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/qiusheng/androiduidemo/User.java
@@ -0,0 +1,10 @@
+package com.qiusheng.androiduidemo;
+
+/**
+ * Created by liling on 2021/3/24 18:28.
+ * <p>
+ * desc：
+ */
+
+public  class User {
+}
